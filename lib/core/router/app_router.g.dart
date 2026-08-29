0fd87bc5cb7af6f2afb49934365e81b1a946ae78
Olssen <olssensujati401@gmail.com>
2026-08-29 09:37:12 +0700

    chore(build): generate updated code with build_runner

--- lib/core/router/app_router.g.dart
+++ lib/core/router/app_router.g.dart
@@ -48,4 +48,4 @@ final class AppRouterProvider
   }
 }
 
-String _$appRouterHash() => r'a49bd04cd259803ec79bdd9dc24eae082ba5f489';
+String _$appRouterHash() => r'b2cc4ccf674d0ab4e1afd632e988b6991d71afe4';
